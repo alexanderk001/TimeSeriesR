91b736e011ac09ef74fa50d1099d9fae5c3933ef
Alexander <alexanderklaus01@gmail.com>
2024-12-20 13:30:48 +0100

    Cleanup: remove unused files

--- scr-document.R
+++
@@ -1,9 +0,0 @@
-#
-# *document*
-# apply as needed.
-#
-
-library(devtools)
-library(roxygen2)
-build_vignettes(pkg = "src/")
-roxygenize(package.dir = "src/", roclets = NULL, load_code = NULL, clean = FALSE)
--- scr-format.R
+++
@@ -1,8 +0,0 @@
-#
-# *package auto formatting*
-# apply as needed.
-#
-
-# if(!require(styler)) install.packages("styler", repos = "https://cloud.r-project.org")
-library(styler)
-styler::style_pkg("src/")
